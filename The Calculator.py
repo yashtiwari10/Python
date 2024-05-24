a7b5457cdca0b363af07cdd96e0d1cafa3c39f04
Yash Tiwari <yt4369496@gmail.com>
2024-05-24 09:18:03 +0530

    Add files via upload

---
+++ The Calculator.py
@@ -0,0 +1,9 @@
+a = 5
+b = 2
+print("The Sum of", a, "and",b, "is :", a + b)
+print("The Subtraction  of", a, "and",b, "is :", a - b)
+print("The Multiplication of", a, "and",b, "is :", a * b)
+print("The Division  of", a, "and",b, "is :", a / b)
+print("The Exponential  of", a, "and",b, "is :", a ** b)
+print("The Floor Division  of", a, "and",b, "is :", a // b)
+print("The Modulus  of", a, "and", b, "is :", a % b) 
